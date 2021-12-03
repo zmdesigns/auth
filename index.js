991487d1cfca1bcb2236c6e869a4c298be032869
zackmdesigns <zackmdesigns@gmail.com>
2021-12-03 11:05:12 -0800

    Enabled use of application/x-www-form-urlencoded

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(helmet());
 
 // parse application/json
 app.use(express.json());
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.status(200).json({ message: 'hi' });
